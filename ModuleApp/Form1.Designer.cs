1acf5c34861253bd6735d78a5ff89d10f98b6280
Anastasia Altyntseva <nastyaltyntseva@gmail.com>
2020-06-16 20:59:21 +0300

    Написала программу для зачета

---
+++ ModuleApp/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace ModuleApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.textBoxKey = new System.Windows.Forms.TextBox();
+            this.buttonEncrypt = new System.Windows.Forms.Button();
+            this.buttonDecrypt = new System.Windows.Forms.Button();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxInput.Location = new System.Drawing.Point(108, 131);
+            this.textBoxInput.Multiline = true;
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxInput.Size = new System.Drawing.Size(274, 230);
+            this.textBoxInput.TabIndex = 0;
+            // 
+            // textBoxKey
+            // 
+            this.textBoxKey.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxKey.Location = new System.Drawing.Point(361, 42);
+            this.textBoxKey.Name = "textBoxKey";
+            this.textBoxKey.Size = new System.Drawing.Size(211, 26);
+            this.textBoxKey.TabIndex = 1;
+            // 
+            // buttonEncrypt
+            // 
+            this.buttonEncrypt.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonEncrypt.Location = new System.Drawing.Point(75, 386);
+            this.buttonEncrypt.Name = "buttonEncrypt";
+            this.buttonEncrypt.Size = new System.Drawing.Size(164, 53);
+            this.buttonEncrypt.TabIndex = 2;
+            this.buttonEncrypt.Text = "Зашифровать";
+            this.buttonEncrypt.UseVisualStyleBackColor = true;
+            this.buttonEncrypt.Click += new System.EventHandler(this.buttonEncrypt_Click);
+            // 
+            // buttonDecrypt
+            // 
+            this.buttonDecrypt.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonDecrypt.Location = new System.Drawing.Point(245, 386);
+            this.buttonDecrypt.Name = "buttonDecrypt";
+            this.buttonDecrypt.Size = new System.Drawing.Size(164, 53);
+            this.buttonDecrypt.TabIndex = 3;
+            this.buttonDecrypt.Text = "Расшифровать";
+            this.buttonDecrypt.UseVisualStyleBackColor = true;
+            this.buttonDecrypt.Click += new System.EventHandler(this.buttonDecrypt_Click);
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.AcceptsReturn = true;
+            this.textBoxOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxOutput.Location = new System.Drawing.Point(474, 131);
+            this.textBoxOutput.Multiline = true;
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.ReadOnly = true;
+            this.textBoxOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxOutput.Size = new System.Drawing.Size(274, 230);
+            this.textBoxOutput.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(284, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Ключ:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(103, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(161, 25);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Введите текст:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(469, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(262, 25);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Преобразованный текст:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(876, 502);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxOutput);
+            this.Controls.Add(this.buttonDecrypt);
+            this.Controls.Add(this.buttonEncrypt);
+            this.Controls.Add(this.textBoxKey);
+            this.Controls.Add(this.textBoxInput);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.TextBox textBoxKey;
+        private System.Windows.Forms.Button buttonEncrypt;
+        private System.Windows.Forms.Button buttonDecrypt;
+        private System.Windows.Forms.TextBox textBoxOutput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ ModuleApp/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ModuleApp
+{
+    public partial class Form1 : Form
+    {
+        private char[] alphabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя".ToCharArray();   //инициализация алфавита
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonEncrypt_Click(object sender, EventArgs e)
+        {
+            string encrypted = EncryptText(textBoxInput.Text, textBoxKey.Text);
+
+            textBoxOutput.Text = encrypted;
+        }
+
+        private void buttonDecrypt_Click(object sender, EventArgs e)
+        {
+            string decrypted = DecryptText(textBoxInput.Text, textBoxKey.Text);
+
+            textBoxOutput.Text = decrypted;
+        }
+
+        private string EncryptText(string text, string key)
+        {
+            string fullKey = GetFullKey(text, key);
+
+            char[] encrypted = text.ToCharArray();
+            int i_key = 0;
+            for (int i = 0; i < text.Length; i++)   // цикл шифровки
+            {
+                char symbol = text[i];
+                if (Char.IsLetter(symbol) && alphabet.Contains(symbol))
+                {
+                    int offset = Char.IsUpper(symbol) ? 0 : 33;
+                    int letterIndex = (GetNInAlphabet(text[i]) + GetNInAlphabet(fullKey[i_key])) % 33 + offset;
+                    encrypted[i] = alphabet[letterIndex];
+                    i_key++;
+                } 
+            }
+
+            return new String(encrypted);
+        }
+
+        private string DecryptText(string text, string key)
+        {
+            string fullKey = GetFullKey(text, key);
+
+            char[] decrypted = text.ToCharArray();
+            int i_key = 0;
+            for (int i = 0; i < text.Length; i++)   // цикл расшифровки
+            {
+                char symbol = text[i];
+                if (Char.IsLetter(symbol) && alphabet.Contains(symbol)){
+                    int offset = Char.IsUpper(symbol) ? 0 : 33;
+                    int letterIndex = (GetNInAlphabet(text[i]) + 33 - GetNInAlphabet(fullKey[i_key])) % 33 + offset;
+                    decrypted[i] = alphabet[letterIndex];
+                    i_key++;
+                } 
+            }
+
+            return new String(decrypted);
+        }
+
+        private int GetNInAlphabet(char letter) // Вычисление индекса буквы в алфавите
+        {
+            int letterIndex = Array.IndexOf(alphabet, letter);
+            return Char.IsUpper(letter) ? letterIndex : letterIndex - 33;
+        }
+
+        private string GetFullKey(string text, string key) 
+        {
+            var onlyLetters = new String(text.Where(Char.IsLetter).ToArray());                        // берем из строки text (введенный текст) только буквы
+            var fullKeyLength = (int)Math.Ceiling((float)(onlyLetters.Length) / key.Length);          // вычисляем длину ключа для всего текста
+            var fullKey = string.Concat(Enumerable.Repeat(key, fullKeyLength));                       // полный ключ для текста (пока что с остатком)
+            fullKey = fullKey.Substring(0, fullKey.Length - (fullKey.Length - onlyLetters.Length));   // полный ключ для текста (убираем излишек)
+
+            return fullKey;
+        }
+    }
+}
